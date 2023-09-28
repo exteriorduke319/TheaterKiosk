c05b7b11893c1a7892c618bb7571ea9835000a39
exteriorduke319 <yarnevictanner@gmail.com>
2023-09-28 09:27:01 -0400

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int age;
+
+        System.out.print("Please enter your age: ");
+        age = scan.nextInt();
+        if (age >= 21) {
+            System.out.println("Please pick up your paper wristband");
+        } else {
+            System.exit(0);
+        }
     }
 }
